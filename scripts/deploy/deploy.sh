05470af85fcfc0e8c3dc7784a193fd7ba8a633b2
faisalzaben <fzaben@terkwaz.com>
2020-12-01 22:51:05 +0200

    Fix deploy step

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,3 +1,3 @@
 ﻿#!/bin/sh
 export IMAGE_NAME=fzaben/capstone-app:$BUILD_NUMBER
-kubectl  --namespace=dep set image deployment/capstone-app-dep capstone-app-dep=$IMAGE_NAME --record=true
+kubectl   --kubeconfig=/root/.kube/config --namespace=dep set image deployment/capstone-app-dep capstone-app-dep=$IMAGE_NAME --record=true
